5df47ad0faab1cadc94a88a4abd21205e02de984
chatur1998 <32766422+chatur1998@users.noreply.github.com>
2019-07-05 16:04:26 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import React, { Component } from 'react';
 import ReduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { StyleSheet, Text, View } from 'react-native';
+import router from './router';
 
 class App extends Component {
-  ComponentWillMount() {
-    fetch('https://akrp-server.herokuapp.com/movies?p=1')
-    .then((response) => response.json())
-    .then((res) => this.setState({ imdb: res }));
-  }
-
   render() {
   return (
-    <Provider store={createStore(reducers)} />
-      <Main />
+    <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
+      <router />
     <Provider>
 
    );

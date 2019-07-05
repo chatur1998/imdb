1edc8e8511dfecdb48b84fdf670bfd138ca60348
chatur1998 <32766422+chatur1998@users.noreply.github.com>
2019-07-05 16:06:40 +0530

    Update MainActions.js

--- MainActions.js
+++ MainActions.js
@@ -1,9 +1,19 @@
 import { Actions } from 'react-native-router-flux';
-import BUTTON_PRESSED from './types';
+import axios from 'axios';
+import { BUTTON_PRESSED, FETCH_MOVIE } from './types';
+
+export const fetchMovies = () => {
+  const movies = axios.get('https://akrp-server.herokuapp.com/movies?p=1');
+  return (dispatch) => {
+    dispatch({ type: FETCH_MOVIE, payload: movies });
+      Actions.details();
+    };
+  };
 
 export const buttonPressed = (id) => {
+  const data = axios.get(`https://akrp-server.herokuapp.com/movies/${id}`);
   return {
     type: BUTTON_PRESSED,
-    payload: id
-  };
+    payload: data
+};
 };

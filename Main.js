942d716b92449571308d43acf3ef1bb868633f1b
chatur1998 <32766422+chatur1998@users.noreply.github.com>
2019-07-11 23:52:02 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,47 +1,51 @@
 import React, { Component } from 'react';
-import { Text, View, ScrollView } from 'react-native';
+import { ScrollView, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { Card, Button } from 'react-native-elements';
-import buttonPressed from '../actions';
+import { buttonPressed, fetchMovies } from '../actions';
+import Details from './Details';
+
 
 class Main extends Component {
   componentWillMount() {
     this.props.fetchMovies();
   }
 
-  onButtonPress(id) {
-    this.props.buttonPressed(this.props.movie.id);
-  }  
+  onButtonPress() {
+    this.props.buttonPressed(this.props.movie._id);
+  }
 
   renderMovies() {
-    return this.props.data.map(movie => {
-      <Details record={movie} />
-
-      <Card
-        key={movie._id}
-        title={movie.original_title}
-        image={{ uri: movie.poster_path }}
-      >
-      <Button
-        onPress={this.onButtonPress.bind(this)}
-        backgroundColor="#03A9F4"
-        title="Know more!!"
-      />
-    </Card>
-  );
-  }
+     //const database = Array.from(this.props.data);
+     return this.props.data.map(movie => {
+      return (
+       <Card
+          key={movie._id}
+          title={movie.original_title}
+          image={{ uri: movie.poster_path }}
+       >
+          <Button
+          onPress={this.onButtonPress.bind(this)}
+          backgroundColor="#03A9F4"
+          title="Know more!!"
+          />
+      </Card>
+    );
+  });
+}
   render() {
     return (
       <ScrollView>
-        {this.renderMovies()};
+        <Text>
+          console.log(this.props.data)
+        </Text>
+        {this.renderMovies()}
       </ScrollView>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return { data: state.movie.imdb }
-}
+const mapStateToProps = state => ({ data: state.movie.imdb });
 
 
-export default connect(null, { buttonPressed, fetchMovies })(Main);
+export default connect(mapStateToProps, { buttonPressed, fetchMovies })(Main);
